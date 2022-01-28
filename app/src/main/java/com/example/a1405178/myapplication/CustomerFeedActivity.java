14969f627c0f0813d5aea74247888d6f73314ee7
Zusia <sushmita.roy96@gmail.com>
2022-01-28 07:57:12 +0530

    Final Commit

---
+++ app/src/main/java/com/example/a1405178/myapplication/CustomerFeedActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1405178.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_feed);
+    }
+}
---
+++ app/src/main/java/com/example/a1405178/myapplication/ManagerFeedActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1405178.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManagerFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manager_feed);
+    }
+}
